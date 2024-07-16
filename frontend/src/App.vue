79aa68151f675b825ace7421a2428cb19eac7db1
sitt-a <sitramohammed75@gmail.com>
2024-07-16 07:11:22 +0300

    update App.vue to resolve error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,15 +6,17 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <NavbarView />
+  <div>
+    <header>
+      <div class="wrapper">
+        <NavbarView />
+      </div>
+    </header>
+    <main class="mt-[35vh] hidden md:block">
+      <Sidebar class="md:fixed left-[7vw]" />
+    </main>
+    <div class="mt-[35vh] md:mt-0 md:ml-[18vw] md:mr-[4vw]">
+      <RouterView />
     </div>
-  </header>
-  <main class="mt-[35vh] hidden md:block">
-    <Sidebar class="md:fixed left-[7vw]" />
-  </main>
-  <div class="mt-[35vh] md:mt-0 md:ml-[18vw] md:mr-[4vw]">
-    <RouterView />
   </div>
 </template>
