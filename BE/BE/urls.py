95994ec8a3fac47b11d54176052163bef4eb1b62
sitt-a <sitramohammed75@gmail.com>
2024-07-16 09:51:51 +0300

    backend

--- BE/BE/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/', include('posts.urls')),
-    # Add other URLs as needed
-]
--- BE/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
-
-urlpatterns = [
-    path('posts/', PostListCreateAPIView.as_view(), name='post-list-create'),
-    path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-detail'),
-]
