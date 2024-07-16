22ab260cb1a589c323044b34fb7c10ef9d51b010
selamyemru <selamyemru6@gmail.com>
2024-07-16 06:25:29 +0300

    update the model

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,45 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.text import slugify
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True, null=True)
     content = models.TextField()
-    image = models.ImageField(upload_to='posts', null=True, blank=True)
-    video = models.FileField(upload_to='posts', null=True, blank=True)
+    media = models.JSONField(blank=True, null=True)
     tags = models.ManyToManyField('Tag', related_name='posts', blank=True)
-    location = models.CharField(max_length=100, null=True, blank=True)
-    audience = models.CharField(max_length=50, choices=[
+    location = models.CharField(max_length=100, blank=True, null=True)
+    audience = models.CharField(max_length=20, choices=[
         ('public', 'Public'),
         ('friends', 'Friends'),
-        ('groups', 'Specific Groups')
+        ('group', 'Group'),
     ], default='public')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
-    post_comments = models.ManyToManyField('Comment',related_name='post_comments', blank=True)
-    def __str__(self):
-        return self.title
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=50, unique=True)
 
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.CharField(max_length=100)
-    text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return f"{self.author.username}'s comment on {self.post.title}"
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
+    like = models.ForeignKey(Like, on_delete=models.CASCADE, null=True, blank=True)
+    message = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    created_at = models.DateTimeField(auto_now_add=True)
