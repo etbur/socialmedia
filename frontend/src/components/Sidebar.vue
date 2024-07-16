c71c398f248fef78cd9e129e1b357bdeafde7b39
selamyemru <selamyemru6@gmail.com>
2024-07-16 06:00:26 +0300

    update sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -5,48 +5,116 @@ import { Icon } from "@iconify/vue";
   <div class="flex md:flex-col space-y-4">
     <router-link
       to="/post"
-      active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+      :class="[
+        $route.path === '/post' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:post-outline" class="inline-block mr-2" /><span
-        class="text-blue-800"
+        class="text-blue-800 hover:text-[#C59728]"
         >New Post</span
       ></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/design' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:design" class="inline-block mr-2" />
-      <span class="text-blue-800">Design</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]"
+        >Design</span
+      ></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/game' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:gamepad-variant" class="inline-block mr-2" />
-      <span class="text-blue-800">Game</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]">Game</span></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/voicetotext' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:voice" class="inline-block mr-2" />
-      <span class="text-blue-800"> Voice to Text</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]">
+        Voice to Text</span
+      ></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/market' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:chart-line" class="inline-block mr-2" />
-      <span class="text-blue-800">Marketing</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]"
+        >Marketing</span
+      ></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/job' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:briefcase" class="inline-block mr-2" />
-      <span class="text-blue-800">Job </span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]">Job </span></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/ad' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:announcement" class="inline-block mr-2" />
-      <span class="text-blue-800">Ad</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]">Ad</span></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  "
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/signlanguage' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:language" class="inline-block mr-2" />
-      <span class="text-blue-800">Sign Language</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]"
+        >Sign Language</span
+      ></router-link
     >
     <h1 class="font-semibold text-xl">Other</h1>
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  " class=""
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/follower' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:account-multiple" class="inline-block mr-2" />
-      <span class="text-blue-800"> Follower</span></router-link
+      <span class="text-blue-800 hover:text-[#C59728]">
+        Follower</span
+      ></router-link
     >
-    <router-link to="" active-class="  border-1 bg-gray-200 px-3 py-1 rounded-md  " class=""
+    <router-link
+      to=""
+      :class="[
+        $route.path === '/student' ? 'bg-[#E2E2E2]' : '',
+        'px-2',
+        'rounded-md',
+      ]"
       ><Icon icon="mdi:school" class="inline-block mr-2" /><span
-        class="text-blue-800"
+        class="text-blue-800 hover:text-[#C59728]"
       >
         Student
       </span></router-link
