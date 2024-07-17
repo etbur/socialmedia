7e8a072f637ba8ae2dd1480e12af89b7324a032d
sitt-a <sitramohammed75@gmail.com>
2024-07-17 07:01:47 +0300

    backend

--- backend/newpost/consumers.py
+++ backend/newpost/consumers.py
@@ -1,10 +1,32 @@
-# import json
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-# from asgiref.sync import async_to_sync
-# from channels.generic.websocket import WebsocketConsumer
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        if self.user.is_anonymous:
+            # Reject the connection
+            await self.close()
+        else:
+            self.group_name = f"user_{self.user.id}"
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
 
-# from .models import Post
-# # class PostConsumer(WebsocketConsumer):
-    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
 
-    
+    async def receive(self, text_data):
+        # Handle incoming WebSocket message if necessary
+        pass
+
+    async def new_notification(self, event):
+        notification = event["notification"]
+        await self.send(text_data=json.dumps({
+            "notification": notification
+        }))
