1655ebac02f599da411dc7483082e547910c72d3
sitt-a <sitramohammed75@gmail.com>
2024-07-17 07:01:24 +0300

    backend

--- backend/newpost/routing.py
+++ backend/newpost/routing.py
@@ -1,7 +1,6 @@
-# from django.urls import re_path
+from django.urls import re_path
+from . import consumers
 
-# from . import consumers
-
-# websocket_urlpatterns = [
-#     re_path(r'ws/post/$', consumers.PostConsumer.as_asgi()),
-# ]
+websocket_urlpatterns = [
+    re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+]
