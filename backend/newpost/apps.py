49b12bfe9449abf2ea38c4869520aae44748f282
Selam <117808245+selamyemru@users.noreply.github.com>
2024-07-23 16:41:39 +0300

    Delete backend directory

--- backend/newpost/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewpostConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newpost'
--- backend/newpost/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-# from rest_framework.routers import DefaultRouter
-from . import views
-urlpatterns=[
-  path('posts',views.PostViewSet.as_view(
-    {'get': 'list'}
-  ) ,name='posts'),
-]
